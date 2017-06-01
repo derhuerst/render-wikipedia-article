6fc559529124ae7c7b6bf21136ca5dc49b401bc5
Jannis R <mail@jannisr.de>
2017-06-01 03:27:55 +0200

    super basic HTML generation

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict'
 
-// todo
+const {parse} = require('wtf_wikipedia')
+const h = require('virtual-dom/h')
+const slugg = require('slugg')
+
+const render = (wikitext) => {
+	const data = parse(wikitext)
+	const out = []
+
+	for (let [heading, content] of data.text) {
+		const text = content.map((chunk) => {
+			// todo: links, end of paragraph
+			return chunk.text
+		}).join(' ')
+
+		out.push(h('h2', {id: slugg(heading)}, heading), text)
+	}
+
+	return h('main', {}, out)
+}
+
+module.exports = render
